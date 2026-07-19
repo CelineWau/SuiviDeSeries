0991e5fc5935bd74c59ffecf341060e53c1b1335
CelineWau <ce.waucheul@gmail.com>
2026-07-19 10:50:43 +0200

    Correction requête presqueFiniesPal : exclusion séries TERMINEE

--- src/main/java/fr/celine/suivideseries/repository/SerieRepository.java
+++ src/main/java/fr/celine/suivideseries/repository/SerieRepository.java
@@ -16,6 +16,7 @@ public interface SerieRepository  extends JpaRepository<Serie, Integer> {
     List<Serie> trouverSeriesParNombreLivresManquants(int livreManquant);
 
     @Query("SELECT s FROM Serie s LEFT JOIN s.livres l WHERE s.statutSerie != fr.celine.suivideseries.enums.StatutSerie.ABANDONNEE " +
+            "AND s.statutSerie != fr.celine.suivideseries.enums.StatutSerie.TERMINEE " +
             "GROUP BY s.idSerie HAVING s.nombreLivreTotal - SUM(CASE WHEN l.statutLivre = fr.celine.suivideseries.enums.StatutLivre.LU THEN 1 ELSE 0 END) <= ?1 " +
             "AND s.nombreLivreTotal - SUM(CASE WHEN l.statutLivre = fr.celine.suivideseries.enums.StatutLivre.LU THEN 1 ELSE 0 END) > 0 " +
             "AND SUM(CASE WHEN l.statutLivre = fr.celine.suivideseries.enums.StatutLivre.A_ACHETER THEN 1 ELSE 0 END) = 0")
